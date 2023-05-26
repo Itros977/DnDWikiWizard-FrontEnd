db748cf62b99e38d1f6c3f2b393084d9438bbf7a
Eric Martínez <EricMartinezProgramador@gmail.com>
2023-05-26 15:17:53 +0200

    Progreso dinámico y números en las barras de progreso

--- Entidades/Jugador.js
+++ Entidades/Jugador.js
@@ -1,8 +1,11 @@
 class Jugador {
-  constructor(nombre, raza, rol, rutaFoto) {
+  constructor(nombre, raza, rol, rutaFoto, hp, mana, xp) {
     this.nombre = nombre;
     this.raza = raza;
     this.rol = rol;
     this.rutaFoto = rutaFoto;
+    this.hp = hp;
+    this.mana = mana;
+    this.xp = xp;
   }
 }
--- Pantallas/DungeonMaster/DungeonMaster.js
+++ Pantallas/DungeonMaster/DungeonMaster.js
@@ -4,9 +4,9 @@ $( document ).ready(function() {
     //$("#divListaJugadores").load("../../Modules/TarjetaJugador/TarjetaJugador.html");
 
     const datosJugadores = [
-        new Jugador("Gimbli", "Enano", "Tanque", "https://s2.ppllstatics.com/ideal/www/multimedia/201712/05/media/cortadas/gimli-ku9B-U50297969827dHF-624x385@Ideal.jpg"),
-        new Jugador("Légolas", "Elfo", "Distancia",  "https://upload.wikimedia.org/wikipedia/en/thumb/2/2b/Legolas600ppx.jpg/220px-Legolas600ppx.jpg"),
-        new Jugador("Aragorn", "Numenoreano", "Paladín",  "https://qph.cf2.quoracdn.net/main-qimg-cbade0d17806e258ac758c2e9dc5e3fb-pjlq")
+        new Jugador("Gimbli", "Enano", "Tanque", "https://s2.ppllstatics.com/ideal/www/multimedia/201712/05/media/cortadas/gimli-ku9B-U50297969827dHF-624x385@Ideal.jpg", "66", "90", "80"),
+        new Jugador("Légolas", "Elfo", "Distancia",  "https://upload.wikimedia.org/wikipedia/en/thumb/2/2b/Legolas600ppx.jpg/220px-Legolas600ppx.jpg", "50", "75", "36"),
+        new Jugador("Aragorn", "Numenoreano", "Paladín",  "https://qph.cf2.quoracdn.net/main-qimg-cbade0d17806e258ac758c2e9dc5e3fb-pjlq", "19", "72", "43")
       ];
 
 
@@ -18,6 +18,17 @@ $( document ).ready(function() {
     
         // Modifica los datos de la instancia
         instancia.find("#nombreTarjetaJugador").text(jugador.nombre);
+        
+        instancia.find("#vidaTarjetaJugador").css("width", jugador.hp + "%");
+        instancia.find("#vidaTarjetaJugador").text(jugador.hp);
+
+        instancia.find("#manaTarjetaJugador").css("width", jugador.mana + "%");
+        instancia.find("#manaTarjetaJugador").text(jugador.mana);
+
+        instancia.find("#xpTarjetaJugador").css("width", jugador.xp + "%");
+        instancia.find("#xpTarjetaJugador").text(jugador.xp + "XP");
+
+
         instancia.find("#razaTarjetaJugador").text(jugador.raza);
         //instancia.find("#rolTarjetaJugador").text(jugador.rol);
         instancia.find("#imagenTarjetaJugador").attr("src", jugador.rutaFoto);
