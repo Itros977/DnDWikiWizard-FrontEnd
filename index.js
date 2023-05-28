a7b42fbc2a5bf654f9e3c1a217c83db691b6a3a0
Eric Martínez <EricMartinezProgramador@gmail.com>
2023-05-28 12:07:03 +0200

    Navegación entre pestañas sin recargar la página!

--- index.js
+++ index.js
@@ -1,4 +1,65 @@
 
 $( document ).ready(function() {
-    $("#divMain").load("/Pantallas/DungeonMaster/DungeonMaster.html");
+
+
+    
+
+    //==============================
+    //= INICIALIZACIÓN DE VENTANAS =
+    //==============================
+
+    $('#divMain').hide();
+    
+    //JUGADORES
+    $.get("/Pantallas/DungeonMaster/DungeonMaster.html", function(data) {
+        var instancia = $(data).clone();
+        $("#divMain").append(instancia);
+    });
+
+    $('#divPrincipalJugadores').show();
+
+    //WIKI
+    $.get("/Pantallas/Wiki/Wiki.html", function(data) {
+        var instancia = $(data).clone();
+        $("#divMain").append(instancia);
+    });
+    
+    $('#divPrincipalWiki').hide();
+
+    $('#divMain').show();
+
+
+
+    //=============================
+    //========== EVENTOS ==========
+    //=============================
+
+    $('#jugadores-link').click(function() {
+        AbrirJugadores();
+    });
+
+    $('#wiki-link').click(function() {
+        AbrirWiki();
+    });
+
+
+
 });
+
+
+function AbrirJugadores(){
+    $('#divPrincipalWiki').hide();
+    $('#divPrincipalJugadores').show();
+
+    $('ul#navBarPartida > li').removeClass('active');
+    $('#jugadores-link').parent().addClass('active');
+
+}
+
+function AbrirWiki(){
+    $('#divPrincipalJugadores').hide();
+    $('#divPrincipalWiki').show();
+
+    $('ul#navBarPartida > li').removeClass('active');
+    $('#wiki-link').parent().addClass('active');
+}
